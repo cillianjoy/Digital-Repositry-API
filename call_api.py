bcfaa9a6ce869901076925f2bfa1c3a4c090718f
cillianjoy <568928+cillianjoy@users.noreply.github.com>
2017-11-27 15:08:04 +0000

    Update script to provide start and row information

--- call_api.py
+++ call_api.py
@@ -13,14 +13,16 @@ import pandas as pd
 
 #import json
 
-# API call, change to suit
+# API call, change to suit.
+# The API returns 200 at a time, to get the next 200 use start. For example &start=200
 url = 'https://digital.library.nuigalway.ie/islandora/rest/v1/solr/dc.relation:robinson*?fl=PID,dc.title,dc.description,mods_subject_topic_ms'
 response = requests.get(url)
 data = response.json()
 
+print("Total datset has " + str(data['response']['numFound']) + " records. This query returned " + str(data['responseHeader']['params']['rows']) + " rows, starting at row " + str(data['response']['start']) + ".")
+
 # Data to Pandas dataframe
 df = json_normalize(data['response']['docs'])
 
-# Print to screen and save to csv
-print(df)
+# Save data to csv
 df.to_csv('data.csv')
